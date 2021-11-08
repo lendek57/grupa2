0fa73fe5354ad4f3041b0edc61500734d1058784
Lena Dekster <lenadekster@gmail.com>
2021-11-08 19:10:12 +0100

    Add periodic border conditions and inheritance

---
+++ src/main/java/simulation/WorldMap.java
@@ -0,0 +1,19 @@
+package simulation;
+
+import java.util.Random;
+
+public class WorldMap extends AbstractWorldMap {
+    private Animal animal;
+    private Random random;
+
+    public WorldMap(int width, int height) {
+        super(width, height);
+        animal = new Animal(new Vector2D(0, 0));
+        random = new Random();
+    }
+
+    @Override
+    public void run() {
+        animal.move(MapDirection.values()[random.nextInt(MapDirection.values().length)]);
+    }
+}
