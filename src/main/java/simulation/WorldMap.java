e52da11911da300a297334e8765afb823c946855
Lena Dekster <lenadekster@gmail.com>
2021-11-08 20:20:31 +0100

    After lab5

--- src/main/java/simulation/WorldMap.java
+++ src/main/java/simulation/WorldMap.java
@@ -1,19 +1,31 @@
 package simulation;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class WorldMap extends AbstractWorldMap {
-    private Animal animal;
+    private static final int ANIMALS_NO = 15, PLANTS_NO = 100;
+    private ArrayList<Animal> animals = new ArrayList<>();
+    private LinkedList<Plant> plants = new LinkedList<>();
     private Random random;
 
     public WorldMap(int width, int height) {
         super(width, height);
-        animal = new Animal(new Vector2D(0, 0));
         random = new Random();
+        for (int i = 0; i < ANIMALS_NO; i++) {
+            animals.add(new Animal(getRandomPosition()));
+        }
+    }
+
+    private Vector2D getRandomPosition() {
+        return new Vector2D(random.nextInt(getWidth()), random.nextInt(getHeight()));
     }
 
     @Override
     public void run() {
-        animal.move(MapDirection.values()[random.nextInt(MapDirection.values().length)]);
+        for (Animal animal : animals) {
+            animal.move(MapDirection.values()[random.nextInt(MapDirection.values().length)]);
+        }
     }
 }
